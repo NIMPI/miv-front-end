3fdda028d3f4d451ff138de9f963ab0cb50365dd
victorbailiot <38817177+victorbailiot@users.noreply.github.com>
2020-06-06 22:25:30 -0400

    Adicionando tela de login
    Adicionado a tela de login sem a parte de conexão com API

--- painel-admin/src/App.vue
+++ painel-admin/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Login.vue'
 
 export default {
   name: 'App',
