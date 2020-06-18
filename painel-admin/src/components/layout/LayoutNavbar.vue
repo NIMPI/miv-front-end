7f90d8bf5dbe51dcd5743a578f69984acad7dd80
victorbailiot <38817177+victorbailiot@users.noreply.github.com>
2020-06-17 23:29:20 -0400

    Adicionando página de inserir dados e navbar do topo

--- painel-admin/src/components/layout/LayoutNavbar.vue
+++ painel-admin/src/components/layout/LayoutNavbar.vue
@@ -1,49 +1,57 @@
 <template>
-  <nav class="navbar navbar-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <router-link :to="{ name: 'home' }">
       <div style="width: 100px; height: 100px;background-color: #117A25; padding: 2%;">
-      <img src="../../assets/logo.png" alt="Logo" class="logo_navbar">
+        <img src="../../assets/logo.png" alt="Logo" class="logo_navbar" />
       </div>
     </router-link>
-    <div>
-      <h2 class="float-left">{{ user.name }}</h2>
-      <img class="avatar_navbar" src="../../assets/images/avatar.png" alt="Avatar">
-      <button class="btn btn-notificatios">
-        <i class="material-icons">keyboard_arrow_down</i>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <div class="nav align-items-center">
+          <i class="ml-5 mr-2 material-icons icon_color_primary">search</i>
+          <div class="input_container">
+            <input type="text" class="awsome_input" placeholder="Pesquisar..." />
+          </div>
+        </div>
+      </ul>
+      <div>
+        <p class="mr-3 d-inline-flex text-center">{{ user.name }}</p>
+        <img class="avatar_navbar" src="../../assets/images/avatar.png" alt="Avatar" />
+        <button class="btn">
+          <i class="material-icons icon_color_primary">keyboard_arrow_down</i>
+        </button>
+      </div>
+      <div class="spacer"></div>
+      <button class="btn">
+        <i class="material-icons icon_color_primary">notifications</i>
+      </button>
+      <button class="btn">
+        <i class="material-icons icon_color_primary">exit_to_app</i>
       </button>
     </div>
-    <button class="btn btn-notificatios">
-      <i class="material-icons">notifications</i>
-    </button>
-    <button class="btn btn-exit">
-      <i class="material-icons">exit_to_app</i>
-    </button>
   </nav>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState('auth', ['user']),
-    routes () {
-      return this.$router.options.routes.filter(route => (
-        route.meta && route.meta.showNavbar
-      ))
+    ...mapState("auth", ["user"]),
+    routes() {
+      return this.$router.options.routes.filter(
+        route => route.meta && route.meta.showNavbar
+      );
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .btn-exit {
-    display: flex;
-    top: 40px;
-    right: 40px;
-  }
-  .btn-notifications{
-    display: flex;
-
-  }
+.btn-exit {
+  display: flex;
+  top: 40px;
+  right: 40px;
+}
 </style>
--- painel-admin/src/pages/watchedlist/Watchedlist.vue
+++ painel-admin/src/pages/watchedlist/Watchedlist.vue
@@ -1,41 +1,58 @@
 <template>
   <div class="container-fluid">
-    <h1 class="mt-3">Watchedlist</h1>
-    <hr>
-
-    <div class="row">
-      <div
-        class="col-3"
-        :key="item.id"
-        v-for="item in watchedlist"
-      >
-        <serie-box :serie="item" />
+    <form>
+      <div alt="name" class="form-group">
+        <label for="exampleInputEmail1">Nome do arquivo:</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="João Silva" />
       </div>
-    </div>
+      <div alt="Date" class="form-group">
+        <label for="exampleInputPassword1">Data de publicação:</label>
+        <input type="date" class="form-control" id="exampleInputPassword1" />
+      </div>
+      <div alt="Descrição" class="form-group">
+        <label for="exampleFormControlTextarea1">Descrição: </label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div alt="Tipo_arquivo" class="form-group">
+        <label for="exampleFormControlSelect1">Tipo de arquivo: </label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>Jornal</option>
+          <option>Áudio</option>
+          <option>Video</option>
+          <option>Livro</option>
+          <option>Documento</option>
+          <option>Outros</option>
+        </select>
+      </div>
+      <div alt="File Input" class="form-group">
+        <label for="exampleFormControlFile1">Example file input</label>
+        <input type="file" class="form-control-file" id="exampleFormControlFile1" />
+      </div>
+      <button type="submit" class="mt-5 btn btn-success nav p-2 pr-3"><i class="material-icons mr-3">done</i>Publicar</button>
+    </form>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
-import SerieBox from '@/components/SerieBox'
+import { mapActions, mapState } from "vuex";
+import SerieBox from "@/components/SerieBox";
 
 export default {
-  name: 'Watchedlist',
+  name: "Watchedlist",
   components: {
     SerieBox
   },
-  mounted () {
-    this.ActionFindWatchedlist()
+  mounted() {
+    this.ActionFindWatchedlist();
   },
   computed: {
-    ...mapState('watchedlist', ['watchedlist'])
+    ...mapState("watchedlist", ["watchedlist"])
   },
   methods: {
-    ...mapActions('watchedlist', ['ActionFindWatchedlist'])
+    ...mapActions("watchedlist", ["ActionFindWatchedlist"])
   }
-}
+};
 </script>
 
 <style>
-
 </style>
